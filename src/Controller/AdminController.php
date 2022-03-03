b3a381a5c495b6947bbe2bd4d2855f2d93a90333
Roess Thomas <thomas_roess@hotmail.fr>
2022-03-03 17:48:11 +0100

    listed extrafields

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactExtrafields;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $extrafieldsRepository = $doctrine->getRepository(ContactExtrafields::class);
+        $extrafields = $extrafieldsRepository->findAll();
+
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('admin/index.html.twig', [
             'user' => $this->getUser(),
+            'extrafields' => $extrafields,
         ]);
     }
 }
